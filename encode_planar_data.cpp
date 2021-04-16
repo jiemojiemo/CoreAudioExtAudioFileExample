7c333dfee2ef7721af3a5acccc2b510dd55696ce
huawei.william <huawei.william@bytedance.com>
2021-04-16 18:54:49 +0800

    add delete

--- encode_planar_data.cpp
+++ encode_planar_data.cpp
@@ -79,34 +79,16 @@ int main(int argc, char* argv[])
     status = ExtAudioFileSetProperty(output_file, kExtAudioFileProperty_ClientDataFormat,
                                      sizeof(input_asbd), &input_asbd);
 
-//    Typically, when an ASBD is being used, the fields describe the complete layout
-//    of the sample data in the buffers that are represented by this description -
-//        where typically those buffers are represented by an AudioBuffer that is
-//    contained in an AudioBufferList.
-//
-//        However, when an ASBD has the kAudioFormatFlagIsNonInterleaved flag, the
-//    AudioBufferList has a different structure and semantic. In this case, the ASBD
-//    fields will describe the format of ONE of the AudioBuffers that are contained in
-//    the list, AND each AudioBuffer in the list is determined to have a single (mono)
-//    channel of audio data. Then, the ASBD's mChannelsPerFrame will indicate the
-//    total number of AudioBuffers that are contained within the AudioBufferList -
-//        where each buffer contains one channel. This is used primarily with the
-//    AudioUnit (and AudioConverter) representation of this list - and won't be found
-//    in the AudioHardware usage of this structure.
-
     assert(status == noErr);
     const int num_frame_out_per_block = 1024;
     AudioBufferList *outputData = (AudioBufferList*)malloc(sizeof(AudioBufferList) + (sizeof(AudioBuffer) * (i_channels - 1)));
 
     // if input_asbd inIsNonInterleaved is true(planar data), mNumberBuffers set to number of channels
     outputData->mNumberBuffers = i_channels;
-    auto* buffers = new AudioBuffer[int(i_channels)];
     for(auto i = 0; i < i_channels; ++i){
-        buffers[i].mNumberChannels = 1;
-        buffers[i].mDataByteSize = sizeof(float) * num_frame_out_per_block;
-        buffers[i].mData = new float[num_frame_out_per_block];
-
-        outputData->mBuffers[i] = buffers[i];
+        outputData->mBuffers[i].mNumberChannels = 1;
+        outputData->mBuffers[i].mDataByteSize = sizeof(float) * num_frame_out_per_block;
+        outputData->mBuffers[i].mData = new float[num_frame_out_per_block];
     }
 
 
@@ -116,7 +98,7 @@ int main(int argc, char* argv[])
         for(int j = 0; j < num_frame_out_per_block; ++j){
             float x = sin(t) * 0.8;
             for(int c = 0; c < i_channels; ++c){
-                auto* in_channel = (float*)(buffers[c].mData);
+                auto* in_channel = (float*)(outputData->mBuffers[c].mData);
                 in_channel[j] = x;
             }
 
@@ -130,6 +112,11 @@ int main(int argc, char* argv[])
     }
 
     ExtAudioFileDispose(output_file);
+
+    for(auto i = 0; i < i_channels; ++i){
+        delete [] ((float*)(outputData->mBuffers[i].mData));
+    }
+    free(outputData);
 
     return 0;
 }
